8e286207e39fc16607e274ad96970c54c1d1bb7c
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-10-23 20:16:32 +0300

    added docker and postgresql

--- internal/api/workout_handler.go
+++ internal/api/workout_handler.go
@@ -1,1 +1,35 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type WorkoutHandler struct {
+}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutId := chi.URLParam(r, "id")
+	if paramsWorkoutId == "" {
+		http.NotFound(w, r)
+		return
+	}
+	workoutId, err := strconv.ParseInt(paramsWorkoutId, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprint(w, "This is the workout Id: ", workoutId)
+}
+
+func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Created a workout \n")
+}
